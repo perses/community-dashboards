900910a87c4af9402b7f876c9ae7f839d830aca9
Hélia Barroso <helia_barroso@hotmail.com>
2025-05-10 19:42:02 +0100

    fix: lint
    Signed-off-by: Hélia Barroso <helia_barroso@hotmail.com>

--- pkg/dashboards/kubernetes/kubelet/kubelet.go
+++ pkg/dashboards/kubernetes/kubelet/kubelet.go
@@ -2,6 +2,7 @@ package kubelet
 
 import (
 	"github.com/perses/community-dashboards/pkg/dashboards"
+	panels "github.com/perses/community-dashboards/pkg/panels/kubernetes"
 	"github.com/perses/community-dashboards/pkg/promql"
 	"github.com/perses/perses/go-sdk/dashboard"
 
@@ -9,33 +10,35 @@ import (
 	listVar "github.com/perses/perses/go-sdk/variable/list-variable"
 )
 
-func BuildKubeletMixin(project string, datasource string, clusterLabelName string) (dashboard.Builder, error) {
-	clusterLabelMatcher := dashboards.GetClusterLabelMatcher(clusterLabelName)
-	return dashboard.New("kubelet",
-		dashboard.ProjectName(project),
-		dashboard.Name("Kubernetes / Kubelet"),
-		dashboard.AddVariable("cluster",
-			listVar.List(
-				labelValuesVar.PrometheusLabelValues("cluster",
-					labelValuesVar.Matchers("up{job=\"kubelet\"}"),
-					dashboards.AddVariableDatasource(datasource),
-				),
-				listVar.DisplayName("cluster"),
-			),
-		),
-		dashboard.AddVariable("instance",
-			listVar.List(
-				labelValuesVar.PrometheusLabelValues("instance",
-					labelValuesVar.Matchers(
-						promql.SetLabelMatchers(
-							"up{job=\"kubelet\"}",
-							[]promql.LabelMatcher{clusterLabelMatcher, {Name: "cluster", Type: "=", Value: "$cluster"}},
-						),
+func BuildKubeletMixin(project string, datasource string, clusterLabelName string) dashboards.DashboardResult {
+	//clusterLabelMatcher := dashboards.GetClusterLabelMatcher(clusterLabelName)
+	return dashboards.NewDashboardResult(
+		dashboard.New("kubelet",
+			dashboard.ProjectName(project),
+			dashboard.Name("Kubernetes / Kubelet"),
+			dashboard.AddVariable("cluster",
+				listVar.List(
+					labelValuesVar.PrometheusLabelValues("cluster",
+						labelValuesVar.Matchers("up{"+panels.GetKubeletMatcher()),
+						dashboards.AddVariableDatasource(datasource),
 					),
-					dashboards.AddVariableDatasource(datasource),
+					listVar.DisplayName("cluster"),
 				),
-				listVar.DisplayName("instance"),
+			),
+			dashboard.AddVariable("instance",
+				listVar.List(
+					labelValuesVar.PrometheusLabelValues("instance",
+						labelValuesVar.Matchers(
+							promql.SetLabelMatchers(
+								"up{"+panels.GetKubeletMatcher(),
+								[]promql.LabelMatcher{{Name: "cluster", Type: "=", Value: "$cluster"}},
+							),
+						),
+						dashboards.AddVariableDatasource(datasource),
+					),
+					listVar.DisplayName("instance"),
+				),
 			),
 		),
-	)
+	).Component("kubernetes")
 }
